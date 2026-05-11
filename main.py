2c55b6ef2e54219b02052e09f81a11b878133214
Leo Durrant <leodurrant@microsoft.com>
2026-05-11 09:33:39 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from openai import AzureOpenAI
+
+endpoint = "https://app-uoaihack6zs3w.azurewebsites.net"
+model_name = "gpt-5.4"
+deployment = "gpt-5.4"
+
+subscription_key = "<your api key>"
+api_version = "2024-12-01-preview"
+
+client = AzureOpenAI(
+    api_version=api_version,
+    azure_endpoint=endpoint,
+    api_key=subscription_key,
+)
+
+response = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful assistant.",
+        },
+        {
+            "role": "user",
+            "content": "I am going to Paris, what should I see?",
+        },
+        {
+            "role": "assistant",
+            "content": "Paris, the capital of France, is known for its stunning architecture, art museums, historical landmarks, and romantic atmosphere. Here are some of the top attractions to see in Paris:\n\n1. The Eiffel Tower: The iconic Eiffel Tower is one of the most recognizable landmarks in the world and offers breathtaking views of the city.\n2. The Louvre Museum: The Louvre is one of the worlds largest and most famous museums, housing an impressive collection of art and artifacts, including the Mona Lisa.\n3. Notre-Dame Cathedral: This beautiful cathedral is one of the most famous landmarks in Paris and is known for its Gothic architecture and stunning stained glass windows.\n\nThese are just a few of the many attractions that Paris has to offer. With so much to see and do, its no wonder that Paris is one of the most popular tourist destinations in the world.",
+        },
+        {
+            "role": "user",
+            "content": "What is so great about #1?",
+        }
+    ],
+    max_completion_tokens=16384,
+    model=deployment
+)
+
+print(response.choices[0].message.content)
